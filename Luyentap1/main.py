a9668ed1fa8817eca08ec331009fb6e905533be3
Tham2007 <phamtham312007@gmail.com>
2026-05-19 21:00:37 +0700

    Add files via upload

---
+++ Luyentap1/main.py
@@ -0,0 +1,42 @@
+# Bai 1
+so_nguyen = 9
+so_thuc = -2
+chuoi ="hello world"
+print("số nguyên:", so_nguyen)
+print("số thực:", so_thuc)
+print("chuỗi:",chuoi)
+# Bai 2
+pi=3.14
+r=5
+chu_vi = 2*pi*r
+print("Chu vi hình tròn là:", chu_vi)
+# Bai 3
+a = int(input("Nhập số thức nhất:"))
+b = int(input("Nhập số thứ hai:"))
+tong = a+b
+hieu = a-b
+tich = a*b
+thuong = a/b
+print("Tổng 2 số là:",tong)
+print("Hiệu 2 số là:",hieu)
+print("Tích 2 số là:",tich)
+print("Thương 2 số là:", thuong)
+# Bai 4
+def sum_two_numbers(a,b):
+    return a+b
+tong = sum_two_numbers(3,4)
+print("Tổng 2 số là", tong)
+# Bai 5
+name = "Thắm"
+age = 19
+average_score = 8.5
+print(type(name))
+print(type(age))
+print(type(average_score))
+age_next_year = age+1
+doubled_score = average_score*2
+print("name:",name)
+print("age:", age)
+print("doubled score:", doubled_score)
+print("age next year:", age_next_year)
+print("average score", average_score)
