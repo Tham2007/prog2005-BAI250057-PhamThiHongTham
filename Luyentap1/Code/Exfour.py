b57211e40fcd9be5d2d8a2d0b18a00689c6d094d
Tham2007 <phamtham312007@gmail.com>
2026-05-19 20:49:17 +0700

    Delete Luyentap1 directory

--- Luyentap1/Code/Exfour.py
+++
@@ -1,4 +0,0 @@
-def sum_two_numbers(a,b):
-    return a+b
-tong = sum_two_numbers(3,4)
-print("Tổng 2 số là", tong)
--- Luyentap1/Code/Exone.py
+++
@@ -1,6 +0,0 @@
-so_nguyen = 9
-so_thuc = -2
-chuoi ="hello world"
-print("số nguyên:", so_nguyen)
-print("số thực:", so_thuc)
-print("chuỗi:",chuoi)
